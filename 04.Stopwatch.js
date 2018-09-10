c978bc0bdec38900c1e6e14e4e14d635f0876a3b
Tedo74 <teddy2@abv.bg>
2018-09-10 15:38:28 +0300

    Edited 04.stopwatch

--- 04.Stopwatch.js
+++ 04.Stopwatch.js
@@ -4,24 +4,24 @@ function stopwatch() {
     startBtn.addEventListener('click', startTimer);
     let stopBtn = document.getElementById('stopBtn');
     stopBtn.addEventListener('click', stopTimer);
-    let seconds = 0;
     let timer = null;
 
     function startTimer() {
         startBtn.disabled = true;
         stopBtn.disabled = false;
-        seconds = 0;
+        let seconds = 0;
+        tick();
         timer = setInterval(tick, 1000);
+
+        function tick() {
+            clock.textContent = `${('0' + Math.floor(seconds / 60)).slice(-2)}:${('0' + Math.floor(seconds % 60)).slice(-2)}`;
+            seconds++;
+        }
     }
 
     function stopTimer() {
         startBtn.disabled = false;
         stopBtn.disabled = true;
         clearInterval(timer);
-    }
-
-    function tick() {
-        clock.textContent = `${('0' + Math.trunc(seconds / 60)).slice(-2)}:${('0' + Math.trunc(seconds % 60)).slice(-2)}`;
-        seconds++;
     }
 }
