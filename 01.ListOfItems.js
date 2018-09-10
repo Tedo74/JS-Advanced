8a20dbc93984da5405b9b58b619dea6096f3de39
Tedo74 <teddy2@abv.bg>
2018-09-10 11:00:13 +0300

    test removed file

--- 01.ListOfItems.js
+++
@@ -1,8 +0,0 @@
-function addItem() {
-    let textToLi = document.getElementById('newItemText').value;
-    let liToInsert = document.createElement('li');
-    liToInsert.textContent = textToLi;
-    let ul = document.getElementById('items');
-    ul.appendChild(liToInsert);
-    document.getElementById('newItemText').value = '';
-}
