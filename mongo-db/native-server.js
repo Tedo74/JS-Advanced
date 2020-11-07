dab55008c2734c432c7f0d5ded7ecdf708dab718
tedo <teddy2@abv.bg>
2020-11-07 23:04:32 +0200

    mongoDb native

---
+++ mongo-db/native-server.js
@@ -0,0 +1,23 @@
+const mongodb = require('mongodb');
+
+connectionStr = 'mongodb://localhost:27017/myDb';
+
+mongodb.MongoClient.connect(
+  connectionStr,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (connectErr, client) => {
+    if (connectErr) {
+      console.log(connectErr);
+      return;
+    }
+    const coll = client.db('myDb').collection('cars');
+    coll.insertOne({ model: 'BMW' });
+    coll
+      .find()
+      .toArray()
+      .then((data) => {
+        console.log(JSON.stringify(data));
+      });
+    // client.close();
+  }
+);
