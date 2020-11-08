9d34b62ab36f0235e6f54c902f7eb78580cb082b
tedo74 <teddy2@abv.bg>
2020-11-08 15:06:57 +0200

    mongoose edit

--- mongo-db/mongoose.js
+++ mongo-db/mongoose.js
@@ -12,10 +12,30 @@ mongoose.connect(
   }
 );
 
-const carSchema = {
+const carSchema = new mongoose.Schema({
   model: { type: String, required: true },
   price: { type: Number, required: false },
+});
+
+carSchema.methods.bestCar = function () {
+  console.log(this.model + ' is the best car');
 };
+
 const Car = new mongoose.model('Cars', carSchema);
-let newCar = new Car({ model: 'Mazda', price: 5000 });
+
+//add new car to db
+let newCar = new Car({ model: 'Peugeot', price: 2000 });
 newCar.save();
+
+//function
+Car.findOne({ model: 'Fiat' }).then((foundedCar) => {
+  foundedCar.bestCar();
+});
+
+//search conditions
+Car.find()
+  .or([{ model: 'Ford' }, { model: 'BMW' }])
+  .sort({ price: -1 })
+  .then((result) => {
+    console.log(result);
+  });
